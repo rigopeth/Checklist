8c59371936c912cafc8af06d4b96acb0dd01c68a
rigo.dominguezg@gmail.com <rigo.dominguezg@gmail.com>
2022-10-21 14:01:27 -0500

    Create class ChecklistItem

---
+++ checklist/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  checklist
+//
+//  Created by Rigoberto Dominguez Garcia on 20/10/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
