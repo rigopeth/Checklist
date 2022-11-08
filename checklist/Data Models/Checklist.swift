6fb9c586e27de977d51ff056b02f7a67a1605809
rigo.dominguezg@gmail.com <rigo.dominguezg@gmail.com>
2022-11-08 17:26:40 -0600

    Changes in Checklist

--- checklist/Data Models/Checklist.swift
+++ checklist/Data Models/Checklist.swift
@@ -11,11 +11,32 @@ class Checklist: NSObject, Codable {
     
     var name = ""
     
+    var iconName = "No Icon"
+    
     var items = [ChecklistItem]()
     
-    init(name: String) {
+    init(name: String, iconName: String = "No Icon") {
         self.name = name
+        self.iconName = iconName
         super.init()
+    }
+    
+    func countUncheckedItems () -> Int {
+        /*var count = 0
+        for item in items where !item.checked {
+            count += 1
+        }
+        return count*/
+        
+        
+        //That’s quite a bit of code for something that’s fairly simple. You
+        // can actually write this in a single line of code:
+        
+        // Functional Programming
+        
+        return items.reduce(0) {
+            cnt,item in cnt + (item.checked ? 0 : 1)
+        }
     }
     
 }
